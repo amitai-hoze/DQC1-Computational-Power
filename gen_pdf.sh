c4b73112e312972efa8b605e8551af0cc0aa220c
Amitai Hoze <amitai.hoze@gmail.com>
2015-07-29 20:38:49 +0000

    Standard update

--- gen_pdf.sh
+++ gen_pdf.sh
@@ -5,5 +5,5 @@ if [ -n "$NIX_GHC" ]; then
   eval $(grep export "$NIX_GHC")
 fi
 
-pdflatex --shell-escape computing_the_jones_polynomial_in_dqc1_beamer.tex >& out
-emacs out & >& /dev/null
+pdflatex --shell-escape computing_the_jones_polynomial_in_dqc1_beamer.tex >& pdf_gen_out.txt
+emacs pdf_gen_out.txt & >& /dev/null

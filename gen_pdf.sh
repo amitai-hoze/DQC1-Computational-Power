c4164019068accc3c9f46bef747922dda554fdf2
Amitai Hoze <amitai.hoze@gmail.com>
2015-07-29 23:17:16 +0000

    Standard update

--- gen_pdf.sh
+++ gen_pdf.sh
@@ -1,9 +1,20 @@
 #! /bin/sh
 
+rm computing_the_jones_polynomial_in_dqc1_beamer.*
+rm -r diagrams
+
+emacs --no-desktop ~/dqc1/dqc1_jones_polynomial.org -l ~/org-mode/elisp/export_subtree.el --kill
+
 NIX_GHC=$(type -p ghc)
 if [ -n "$NIX_GHC" ]; then
   eval $(grep export "$NIX_GHC")
 fi
 
-pdflatex --shell-escape computing_the_jones_polynomial_in_dqc1_beamer.tex >& pdf_gen_out.txt
-emacs pdf_gen_out.txt & >& /dev/null
+pdflatex --shell-escape ~/dqc1/computing_the_jones_polynomial_in_dqc1_beamer.tex >& pdf_gen_out.txt
+echo "Finished"
+if grep -q failed ~/dqc1/pdf_gen_out.txt; then
+    emacs ~/dqc1/pdf_gen_out.txt
+else
+    kde-open ~/dqc1/computing_the_jones_polynomial_in_dqc1_beamer.pdf    
+fi
+  
